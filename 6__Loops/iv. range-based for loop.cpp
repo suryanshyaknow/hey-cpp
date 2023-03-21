b05a57eb74d160b0821333059b090c3fdc98fa36
Suryansh <suryanshgrover1999@gmail.com>
2023-03-21 13:59:55 +0530

    implemented range-based for loop and structures

--- 6__Loops/iv. range-based for loop.cpp
+++ 6__Loops/iv. range-based for loop.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+struct Pair;                // structure prototype
+struct Pair getMaxAndMin(); // function prototype
+
+struct Pair
+{
+    int max;
+    int min;
+};
 
 int main()
 {
@@ -20,7 +28,56 @@ int main()
     in the array using a range-based for loop. The program should prompt the user to enter the number of
     integers in the array and then the integers themselves.
     */
-
-
+    struct Pair max_min = getMaxAndMin();
+    cout << "The maximum and minimum values are " << max_min.max << " and " << max_min.min << " respectively!" << endl;
     return 0;
+}
+
+struct Pair getMaxAndMin()
+{
+    struct Pair result; // Will hold on to the maximum and minimum values
+    int size;
+
+    cout << "Enter the desired size of the array: ";
+    cin >> size;
+
+    int arr[size]; // Declared an array of the size `size`
+    int idx = 0; 
+
+    // Fetching elements from the console entered by the user
+    while (idx < size)
+    {
+        cout << "\nEnter the element at the index " << idx << " : ";
+        cin >> arr[idx];
+        idx++;
+    }
+
+    // Displaying the elements of the array the user entered
+    cout << "\nDisplaying the array you formed: ";
+    for (int ele : arr)
+    {
+        cout << ele << "\t";
+    }
+    cout << endl;
+
+    // Finding the largest element
+    result.max = arr[0];
+    for (int ele : arr)
+    {
+        if (result.max < ele)
+        {
+            result.max = ele;
+        }
+    }
+
+    // Finding the smallest element
+    result.min = arr[0];
+    for (int ele : arr)
+    {
+        if (result.min > ele)
+        {
+            result.min = ele;
+        }
+    }
+    return result;
 }
